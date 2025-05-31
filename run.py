b86ddcf674998f0fc7997c95fc4c6c37aacbbb23
Kolya.F <fedorov.kolya.cat@gmail.com>
2025-05-31 19:14:48 +0200

    tests auf run.py

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home_page.html")
+    return render_template("buchstaben-nudeln-web/app/templates/home_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
