60156eb36724b18044a9a9af057379d53cb67dd2
tvme <ersan@solist.ru>
2025-09-20 19:12:31 +0200

    Refactor SQLALCHEMY_DATABASE_URI to ensure proper format for PostgreSQL URLs

--- nuudel_app/config.py
+++ nuudel_app/config.py
@@ -9,11 +9,11 @@ class Config:
 
     postgres_url = os.getenv('POSTGRES_URL', 'sqlite:///nuudel.db')
     # Исправляем формат URL для SQLAlchemy
-    if postgres_url.startswith('postgres://'):
-        print(f"Fixing postgres URL format for SQLAlchemy {postgres_url[:30]}...")  
-        postgres_url = postgres_url.replace('postgres://', 'postgresql://', 1)
-        print(f"Fixed URL: {postgres_url[:30]}...")
-    SQLALCHEMY_DATABASE_URI = postgres_url
+    # if postgres_url.startswith('postgres://'):
+    #     print(f"Fixing postgres URL format for SQLAlchemy {postgres_url[:30]}...")  
+    #     postgres_url = postgres_url.replace('postgres://', 'postgresql://', 1)
+    #     print(f"Fixed URL: {postgres_url[:30]}...")
+    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URL', 'sqlite:///nuudel.db').replace('postgres://', 'postgresql://')
 
     SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URL', 'sqlite:///nuudel.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
